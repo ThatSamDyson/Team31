e053287fde56e1c490b9e50daa73fe9d9a232952
Samuel Dyson <92155845+ThatSamDyson@users.noreply.github.com>
2024-03-11 16:20:03 +0000

    First Push
    This is the first push

---
+++ Source/Team31/Team31.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Team31.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Team31, "Team31" );
---
+++ Source/Team31/Team31GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Team31GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TEAM31_API ATeam31GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
